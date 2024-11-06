7a1351ebe2b9faaa1d9f8c9ac9100be3247f54d3
Koxuvar <connor@mmvdesigns.com>
2024-11-06 03:54:56 -0600

    trying newthings

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,15 @@
 use shiva::core::TransformerTrait;
 
 fn main() {
-    let xml_file = "/home/connor/Documents/coding/rust_projects/ma_2_pdf/testfile/Safe/testing.xml";
-    let pdf_file = "/home/connor/Documents/coding/rust_projects/ma_2_pdf/testfile/Safe/out.md";
+    let file_start = "/home/connor/Documents/coding/rust_projects/ma_2_pdf/testfile/Safe/";
+    let xml_file = String::from(file_start.to_owned() + "testing.xml");
+    let pdf_file = String::from(file_start.to_owned() + "out.md");
     
     
     let input_vec = std::fs::read(xml_file).unwrap();
     let input_bytes = bytes::Bytes::from(input_vec);
     let document = shiva::xml::Transformer::parse(&input_bytes).unwrap();
-    let output_bytes = shiva::html::Transformer::generate(&document).unwrap();
+    let output_bytes = shiva::pdf::Transformer::generate(&document).unwrap();
 
     std::fs::write(pdf_file, output_bytes).unwrap();
 }
